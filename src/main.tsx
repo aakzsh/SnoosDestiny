a496a85089ac3bda137a6b412a852df7610e4617
aakzsh <aakashferrari@gmail.com>
2024-12-11 17:21:18 +0530

    add ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,59 +1,86 @@
-import { Devvit, useState } from '@devvit/public-api';
+import { Devvit, useState, useAsync, Context } from "@devvit/public-api";
 
-Devvit.configure({
-  http: true,
-});
 
-Devvit.addSettings([
-  {
-    name: 'open-ai-api-key',
-    label: 'Open AI API key',
-    type: 'string',
-    isSecret: true,
-    scope: 'app',
-  },
-]);
+Devvit.configure({http: true, realtime: true})
+Devvit.addCustomPostType({
+  name: "Say Hello",
+  render: () => {
+    const [counter, setCounter] = useState(0);
+    const [response, setResponse] = useState("hello");
+    const [answer, setAnswer] = useState("default")
 
-async function fetchResponse(context: Devvit.Context): Promise<string> {
+    async function fetchResponse(): Promise<string> {
   try {
-    // const apiKey = "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"
-
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/', {
       headers: {
         'Content-Type': 'application/json',
-        // Authorization: `Bearer ${apiKey}`,
       },
-      method: 'GET',
+      method: 'GET'
     });
 
     const json = await res.json();
-
-    return json?.title?.length > 0 ? json?.title : 'No response';
+console.log(json)
+    return json?.title?.length > 0 ? json?.title: 'No response';
   } catch (e: any) {
     console.log('Fetch error ', e);
     return e.toString();
   }
 }
 
-Devvit.addCustomPostType({
-  name: 'Devvit - Ask GPT',
-  render: (context) => {
-    const [answer, setAnswer] = useState<string>('');
-
-    async function onPress() {
-      const response = await fetchResponse(context);
+    
+ async function onPressY() {
+   // print()
+  console.log("called")
+      const response = await fetchResponse();
+   
+  console.log(response)
       setAnswer(response || 'No Response');
-    }
+}
 
+
+    // const height = height
     return (
-      <blocks height="tall">
-        <vstack alignment="center middle" height="100%" gap="large">
-          <button appearance="primary" onPress={onPress}>
-            {'Ask GPT'}
-          </button>
-          <text wrap>{answer}</text>
-        </vstack>
-      </blocks>
+      <zstack width="100%" height="100%">
+        <image
+          url="reddit-bg.png"
+          resizeMode="cover"
+          imageHeight="256px"
+          imageWidth="256px"
+          width="100%"
+          height="100%"
+        />
+        <vstack padding="small">
+            <text size="xxlarge" weight="bold" color="#262322">
+              {"Snoo's Destiny"}
+            </text>
+          </vstack>
+          <vstack alignment="middle end" height="100%" width="100%" padding="medium" >
+            <vstack alignment="center middle" padding="small" backgroundColor="#F6F6F680" cornerRadius="small" >
+              <text color="#262322" size="medium">Welcome to Snoo’s Destiny!</text>
+              <text color="#262322" size="medium">Every day, a new riddle.</text>
+              <text color="#262322" size="medium">Are you ready to solve it and earn</text>
+              <text color="#262322" size="medium">points on the leaderboard?</text>
+            </vstack>
+            <vstack padding="small">
+            <button textColor="#FFF6EC" appearance="success" size="medium">Play Now</button>
+            </vstack>
+          </vstack>
+          <hstack alignment="bottom center" height="100%" width="100%" padding="medium">
+            <button textColor="#FFF6EC" appearance="success" size="small" onPress={()=>onPressY()}>Home</button>
+            <spacer size="medium" />
+            <button textColor="#FFF6EC" appearance="media" size="small">Chapters</button>
+            <spacer size="medium" />
+            <button textColor="#FFF6EC" appearance="media" size="small">Leaderboard</button>
+          </hstack>
+          <text>{answer}</text>
+        {/* <vstack width="100%" height="100%"> */}
+          
+        
+          
+          
+        {/* </vstack> */}
+      </zstack>
+      
     );
   },
 });
